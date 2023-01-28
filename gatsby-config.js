6bd057794b2437be73bbcffe8b37f8179918c3fe
yuichi04 <sparklingengineer.yuya04gen@gmail.com>
2023-01-28 18:16:59 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Apollo Client Tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
